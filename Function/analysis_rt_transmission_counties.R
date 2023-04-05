f5f953e9ded2ee09be3c91fb0956fdb65ac9e0b1
sina mokhtar <sinamokhtar@gmail.com>
2023-04-05 11:11:57 -0600

    add density and point ggplot

--- Function/analysis_rt_transmission_counties.R
+++ Function/analysis_rt_transmission_counties.R
@@ -40,3 +40,119 @@ plot(rt_counties_TranRisk %>%
   group_by(date) %>%
   summarize(meanTotalRt = mean(Rt)))
 
+
+
+
+
+
+
+# Density Plot limited rt 3 weeks later
+names(rt_counties_TranRisk)
+fig = ggplot(data = rt_counties_TranRisk)+
+  geom_density(aes(x=Rt_next3weeks,
+                   y=..density..,
+                   fill=community_transmission_level,
+                   group=community_transmission_level),
+               color="grey",
+               alpha=0.6)+
+  #scale_x_continuous(trans = "log10")+
+  scale_linetype("")+
+  scale_color_manual("CDC\nRisk Level",
+                     values=c("DodgerBlue","Yellow","Orange","Red"))+
+  scale_fill_manual("CDC\nRisk Level",
+                    values=c("DodgerBlue","Yellow","Orange","Red"))+
+  theme_bw()+
+  theme(legend.position = "top",
+        axis.text.y = element_blank(),
+        panel.grid = element_blank(),
+        strip.text = element_text(size = 12))+
+  labs(title = "Density of 3 weeks later Reproduction Number for Transmission Risk Level groups")+
+  xlab("Transmision Risk Level")+
+  annotation_logticks(side="b")
+
+ggsave("Figures/rt_transmissionRisk_46counties_density.jpg",
+       fig, 
+       height=6,
+       width=8,
+       scale=1.65)
+
+
+
+
+# Density Plot limited rt 3 weeks later
+fig = ggplot(data = rt_counties_TranRisk)+
+  geom_density(aes(x=Rt_next3weeks,
+                   y=..density..,
+                   fill=community_transmission_level,
+                   group=community_transmission_level),
+               color="grey",
+               alpha=0.6)+
+  #scale_x_continuous(trans = "log10")+
+  scale_linetype("")+
+  scale_color_manual("CDC\nRisk Level",
+                     values=c("DodgerBlue","Yellow","Orange","Red"))+
+  scale_fill_manual("CDC\nRisk Level",
+                    values=c("DodgerBlue","Yellow","Orange","Red"))+
+  theme_bw()+
+  theme(legend.position = "top",
+        axis.text.y = element_blank(),
+        panel.grid = element_blank(),
+        strip.text = element_text(size = 12))+
+  labs(title = "Density of 3 weeks later Reproduction Number for Transmission Risk Level groups(Rt between 0,3)")+
+  xlab("Transmision Risk Level")+
+  xlim(c(0,3))+
+  annotation_logticks(side="b")
+
+ggsave("Figures/rt_transmissionRisk_46counties_density_limitRt.jpg",
+       fig, 
+       height=6,
+       width=8,
+       scale=1.65)
+
+
+
+
+
+# point for rt 3 weeks later limit between 0 and 3
+names(rt_counties_TranRisk)
+fig = ggplot(data = rt_counties_TranRisk)+
+    geom_point(aes(x=date,
+                   y=Rt_next3weeks,
+                   color=community_transmission_level,
+                   group=county_name),
+               alpha=.7)+
+    theme_bw()+
+    labs(title = "3 weeks later Reproduction Number for Transmission Risk Level groups county level (rt betweem 0 and 3)")+
+    xlab("")+
+    ylab("")+
+    ylim(c(0,3))+
+    scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y")
+  
+  ggsave("Figures/rt3week_transmissionRisk_46counties_point_limitedrt.jpg",
+         fig, 
+         height=6,
+         width=8,
+         scale=1.65)
+
+  
+  
+  
+# point for rt 3 weeks later
+  fig = ggplot(data = rt_counties_TranRisk)+
+    geom_point(aes(x=date,
+                   y=Rt_next3weeks,
+                   color=community_transmission_level,
+                   group=county_name),
+               alpha=.7)+
+    theme_bw()+
+    labs(title = "3 weeks later Reproduction Number for Transmission Risk Level groups county level")+
+    xlab("")+
+    ylab("")+
+    #ylim(c(0,3))+
+    scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y")
+  
+  ggsave("Figures/rt3week_transmissionRisk_46counties_point.jpg",
+         fig, 
+         height=6,
+         width=8,
+         scale=1.65)
