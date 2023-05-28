df9a80c01ce2879ee1820f56a38e13177883b5a0
sina mokhtar <sinamokhtar@tamu.edu>
2023-05-28 02:16:05 -0500

    for loop 68:

--- compute_rt_AUROC_total.r
+++ compute_rt_AUROC_total.r
@@ -81,7 +81,7 @@ columnNames = c("date",
 compared_counties = data.frame(matrix(nrow = 0,
                                       ncol=length(columnNames)))
 
-for (date_index in 1:length(dates)){
+for (date_index in 68:length(dates)){
   
   selected_counties = dplyr::filter(data,
                                     date == dates[date_index])
